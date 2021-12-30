f9a8d0666a459fbcd1fcd9379f3d04075dedd824
prn436 <itsmepradip@gmail.com>
2021-12-29 19:10:43 -0800

    add execute_git.sh

---
+++ execute_git.sh
@@ -0,0 +1,9 @@
+git config --global user.email "itsmepradip@gmail.com"
+git config --global user.name "prn436"
+git init
+git status
+git add iris.csv
+git status
+git commit -m "add iris.csv"
+git remote add origin https://github.com/prn436/dec29_1.git
+git push -u origin master
